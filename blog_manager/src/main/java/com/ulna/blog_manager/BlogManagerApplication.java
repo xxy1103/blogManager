01253ba0f0a32cb3e9b7c78aa06b9b0aa519b695
ulna <xianxingyuan520@outlook.com>
2025-05-05 11:07:30 +0800

    first commit

---
+++ blog_manager/src/main/java/com/ulna/blog_manager/BlogManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ulna.blog_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogManagerApplication.class, args);
+	}
+
+}
