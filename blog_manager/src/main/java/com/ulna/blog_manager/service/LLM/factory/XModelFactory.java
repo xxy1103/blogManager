4cf1f9f5114a6c7c8543fa28b4209f413f15d80f
ulna <xianxingyuan520@outlook.com>
2025-05-12 19:16:17 +0800

    增加属性

--- blog_manager/src/main/java/com/ulna/blog_manager/service/LLM/factory/XModelFactory.java
+++ blog_manager/src/main/java/com/ulna/blog_manager/service/LLM/factory/XModelFactory.java
@@ -3,9 +3,27 @@ package com.ulna.blog_manager.service.LLM.factory;
 import com.ulna.blog_manager.service.LLM.LLMinterface.LLM;
 import com.ulna.blog_manager.service.LLM.api.XModel;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ulna.blog_manager.Config.Config;
+
 public class XModelFactory implements FactoryInterface {
+
+    private String APIKey;
+    private final String APIUrl = "https://spark-api-open.xf-yun.com/v2/chat/completions";
+    private final String model = "x1";
+
+    /**
+     * 构造函数
+     * @param config 配置类
+     */
+    public XModelFactory(Config config) {
+        this.APIKey = config.getXModelAPIKey();
+    }
+
+
     @Override
-    public LLM createLLM(String APIKey, String APIUrl, String model) {
+    public LLM createLLM() {
         return new XModel(APIKey, APIUrl, model);
     }
 
