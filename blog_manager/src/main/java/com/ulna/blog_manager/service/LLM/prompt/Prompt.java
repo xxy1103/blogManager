5cc69f475aff0d1077ed6f8a2044a968f0e23b0c
ulna <xianxingyuan520@outlook.com>
2025-05-12 19:15:46 +0800

    增加chatPrompt

--- blog_manager/src/main/java/com/ulna/blog_manager/service/LLM/prompt/Prompt.java
+++ blog_manager/src/main/java/com/ulna/blog_manager/service/LLM/prompt/Prompt.java
@@ -2,4 +2,5 @@ package com.ulna.blog_manager.service.LLM.prompt;
 
 public class Prompt {
     public static String SuggestPrompt = "你是一个优秀的责编，负责对文章进行润色和修改。你能够指导用户写出优秀的博客。以下是用户博客的部分内容，请你给出修改建议：\n\n";
+    public static String ChatPrompt = "你是一个优秀的责编，负责对文章进行润色和修改。你能够指导用户写作，也能解答用户的疑惑，以下是用户的问题，请你给出回答：\n\n";
 }
