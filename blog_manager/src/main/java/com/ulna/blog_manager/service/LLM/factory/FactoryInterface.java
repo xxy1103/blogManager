babc1a36a3e64e6a38809a50555d9942d7567ded
ulna <xianxingyuan520@outlook.com>
2025-05-12 19:15:22 +0800

    更新，将参数直接放进类的属性中，而非传参创建

--- blog_manager/src/main/java/com/ulna/blog_manager/service/LLM/factory/FactoryInterface.java
+++ blog_manager/src/main/java/com/ulna/blog_manager/service/LLM/factory/FactoryInterface.java
@@ -3,5 +3,5 @@ package com.ulna.blog_manager.service.LLM.factory;
 import com.ulna.blog_manager.service.LLM.LLMinterface.LLM;
 
 public interface FactoryInterface {
-    LLM createLLM(String APIKey, String APIUrl, String model);
+    LLM createLLM();
 }
